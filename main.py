d98523185516db44f3a721f5cfc925a9b1b0f8ff
11101001101000 <t-h-0928@naver.com>
2022-01-30 01:05:50 +0900

    update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
 infn = False
 ccmd = input ('custom command: ')
+pt = input ('executable type: ')
 while infn == False:
     ecode = input('python code to execute: ')
     if ecode == 'infn.':
         infn = True
     else:
-        os.system('echo ' + ecode + ' >> modules\\' + ccmd)
+        os.system('echo ' + ecode + ' >> modules\\' + ccmd + '.' + pt)

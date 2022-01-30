34541de38f72b6830916d4c3c6fad11f14bff619
11101001101000 <t-h-0928@naver.com>
2022-01-31 00:03:23 +0900

    commit

--- setupl.py
+++
@@ -1,5 +0,0 @@
-import os
-os.system ('echo import os > command.py')
-os.system ('echo end = 0 >> command.py')
-os.system ('echo while end == 0: >> command.py')
-os.system ('echo \ \ \ \ cmd = input \(\'command: \'\) >> command.py')

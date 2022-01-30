6c05704a2286c805a934ab4b1018a6112e2c437e
111101001101000 <a>
2022-01-30 21:22:55 +0900

    add multiple line support

--- csk.py
+++ csk.py
@@ -8,11 +8,22 @@ while end == False:
         ecode = input('python code to run(use os.system() to run system commands): ')
         if ecode == 'exitc':
             exit()
-        if ecode == 'infn.':
-            infn = True
-        else:
-            if ecode == '':
-                os.system ('echo \'\' >> command.py')
-            else:
-                os.system ('echo     if cmd == \'' + ccmd + '\': >> command.py')
-                os.system ('echo         ' + ecode + ' >> command.py')
+        if ecode == 'mlc.':
+            fline = 1
+            while infn == False:
+                ecode = input ('code: ')
+                indt = input ('Does the code need indentation?(y/n): ')
+                if ecode == 'exitc':
+                    exit()
+                if ecode == 'infn.':
+                    fline = 2
+                    infn = True
+                if fline == 1:
+                    os.system ('echo     if cmd == \'' + ccmd + '\': >> command.py')
+                    os.system ('echo         ' + ecode + ' >> command.py')
+                    fline = 0
+                if fline == 0:
+                    if indt == 'y':
+                        os.system ('echo             ' + ecode + ' >> command.py')
+                    else:
+                        os.system ('echo         ' + ecode + ' >> command.py')

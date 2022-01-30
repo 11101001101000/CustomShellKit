702d9bfd9efc6e81fc352a1d12eb63a64bb3005b
11101001101000 <t-h-0928@naver.com>
2022-01-31 00:05:50 +0900

    Revert "commit"
    This reverts commit 34541de38f72b6830916d4c3c6fad11f14bff619.

---
+++ command.py
@@ -0,0 +1,10 @@
+import os
+end = 0
+while end == 0:
+cmd = input (command: )
+if cmd == a:
+a = input ()
+if cmd == a:
+a = input()
+if a == 'a':
+    print ('a')
---
+++ setup.py
@@ -0,0 +1,5 @@
+import os
+os.system ('echo import os > command.py')
+os.system ('echo end = 0 >> command.py')
+os.system ('echo while end == 0: >> command.py')
+os.system ('echo     cmd = input (\'command: \') >> command.py')
